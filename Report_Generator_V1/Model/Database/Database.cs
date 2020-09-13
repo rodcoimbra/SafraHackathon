ce7baf592001107522a5a44c206f8c796a69e265
Luiz <lg.motta@hotmail.com>
2020-09-13 16:13:45 -0300

    LG: Added password to db in App.config, update connectionString password to db, update insertSQL in Get_Accounts, update Set_Account entirelly.

--- Report_Generator_V1/Model/Database/Database.cs
+++ Report_Generator_V1/Model/Database/Database.cs
@@ -1,6 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
+using Report_Generator_V1.Properties;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +23,7 @@ namespace Report_Generator_V1.Model.Database
 
         private String Set_connectionString()
         {
-            return ConnectionString = @"Persist Security Info = False; server = localhost; database = safra; uid = root; pwd = safra";
+            return ConnectionString = String.Format(@"Persist Security Info = False; server = localhost; database = safra; uid = root; pwd = {0}", ConfigurationManager.AppSettings["pwdDb"]);
         }
 
         public ReturnStructure Get_Accounts()
@@ -29,7 +31,9 @@ namespace Report_Generator_V1.Model.Database
             ReturnStructure returnStructure = new ReturnStructure() { Data = null, Message = "", Status = true };
             List<Account> data = new List<Account>();
             Account row;
-            string insertSQL = "SELECT * FROM teste";
+            string insertSQL = "SELECT * FROM modelo " +
+                               "WHERE execution_time = (select max(execution_time) from safra.modelo)";
+
 
             using (MySqlConnection connection = new MySqlConnection(ConnectionString))
             {
@@ -98,71 +102,29 @@ namespace Report_Generator_V1.Model.Database
         public ReturnStructure Set_Accounts(List<Account> accounts_to_insert)
         {
             ReturnStructure returnStructure = new ReturnStructure() { Data = null, Message = "", Status = true };
-            List<Account> data = new List<Account>();
-            Account row;
             StringBuilder insertSQL = new StringBuilder("INSERT INTO transferencias(conta, entrada, saida) VALUES ");
 
-            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+            try
             {
-
-                List<string> Rows = new List<string>();
-                for (int i = 0; i < accounts_to_insert.Count; i++)
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
                 {
-                    Rows.Add(string.Format("('{0}','{1}','{2}')", accounts_to_insert[i].Description, accounts_to_insert[i].Balance_in, accounts_to_insert[i].Balance_out));
-                }
-
-                insertSQL.Append(string.Join(",", Rows));
-                insertSQL.Append(";");
-
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand(insertSQL.ToString(), connection))
-                {
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.ExecuteNonQuery();
-                }
-
-                using (MySqlCommand command = new MySqlCommand(insertSQL.ToString()))
-                {
-
-                    command.Connection = connection;
-
                     try
                     {
-                        connection.Open();
-
-                        using (MySqlDataReader reader = command.ExecuteReader())
+                        List<string> Rows = new List<string>();
+                        for (int i = 0; i < accounts_to_insert.Count; i++)
                         {
-                            while (reader.Read())
-                            {
-                                row = new Account();
-
-                                if (reader.FieldCount == row.Account_fields_count)
-                                {
-                                    row.Description = reader[0].ToString();
-
-                                    Double.TryParse(reader[1].ToString(), out double value_in);
-                                    row.Balance_in = value_in;
-
-                                    Double.TryParse(reader[2].ToString(), out double value_out);
-                                    row.Balance_out = value_out;
-
-                                    row.Cluster = reader[3].ToString();
-
-                                    Double.TryParse(reader[4].ToString(), out double value_id_exec);
-                                    row.id_exec = value_id_exec;
-
-                                    DateTime.TryParse(reader[5].ToString(), out DateTime value_date_exec);
-                                    row.time_exec = value_date_exec;
-
-
-                                }
-
-
-                                data.Add(row);
-                            }
+                            Rows.Add(string.Format("('{0}','{1}','{2}')", accounts_to_insert[i].Description, accounts_to_insert[i].Balance_in, accounts_to_insert[i].Balance_out));
                         }
 
-                        returnStructure.Data = data;
+                        insertSQL.Append(string.Join(",", Rows));
+                        insertSQL.Append(";");
+
+                        connection.Open();
+                        using (MySqlCommand command = new MySqlCommand(insertSQL.ToString(), connection))
+                        {
+                            command.CommandType = System.Data.CommandType.Text;
+                            command.ExecuteNonQuery();
+                        }
 
                     }
                     catch (Exception ex)
@@ -175,8 +137,12 @@ namespace Report_Generator_V1.Model.Database
                         if (connection.State == System.Data.ConnectionState.Open)
                             connection.Close();
                     }
-
                 }
+            }
+            catch (Exception ex)
+            {
+                returnStructure.Status = false;
+                returnStructure.Message = ex.Message;
             }
 
             return returnStructure;
